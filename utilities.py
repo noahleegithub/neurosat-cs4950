ff0f5449bbbb85d096fd7484c6d2cfb89d467664
noahleegithub <noah.v.lee@gmail.com>
2022-04-27 18:08:00 -0400

    include ndcg metric

--- utilities.py
+++ utilities.py
@@ -4,6 +4,7 @@ from torch import Tensor
 from typing import Sequence, Tuple
 from sympy import Symbol
 from sympy.logic.boolalg import BooleanFunction, And, Or, Not, Implies
+from sympy.logic.utilities import dimacs
 from relaxations import FuzzyRelaxation, relaxation_types
 
 def dimacs_to_adjacency(in_fname: str, sparse: bool=True):
@@ -60,17 +61,15 @@ def direct_sum(matrices: Sequence[Tensor]) -> Tensor:
 
 
 def compute_acc(votes, sats, assignments, formulas, device):
-    satisfying_assignments_found = 0 # if the problem was SAT and a valid assigment was found
-    relaxation = relaxation_types['godel']
+    relaxation = relaxation_types['product']
     votes = torch.where(votes > 0.5, 1., 0.)
     correct_sat_predictions = torch.sum(votes == sats)
 
-    for sat, assignment, formula in zip(sats, assignments, formulas):
-        if sat == 1:
-            assignment = torch.where(assignment > 0.5, 1., 0.)
-            if relax_sympy(formula, relaxation, assignment, device) == 1:
-                satisfying_assignments_found += 1
-    return correct_sat_predictions, satisfying_assignments_found
+    assignments = torch.where(assignments > 0.5, 1., 0.)
+    satisfactions = torch.tensor([relax_sympy(f, relaxation, a, device) for a, f in zip(assignments, formulas)], 
+            device=device)
+
+    return correct_sat_predictions, torch.sum(satisfactions * sats) 
 
 def extract_int_from_str(s):
     """ Get the integer that is present in the given string. 
@@ -94,23 +93,23 @@ def relax_sympy(expr: BooleanFunction, relaxation_t: FuzzyRelaxation, values: to
         children = expr.args
         ret_value = torch.tensor(1.).to(device)
         for child in children:
-            ret_value = relaxation_t.conjunction(ret_value, relax_sympy(child, relaxation_t, values, device))
+            ret_value = relaxation_t.conjunction(ret_value, relax_sympy(child, relaxation_t, values, device), device)
         return ret_value
     elif isinstance(expr, Or):
         children = expr.args
         ret_value = torch.tensor(0.).to(device)
         for child in children:
-            ret_value = relaxation_t.disjunction(ret_value, relax_sympy(child, relaxation_t, values, device))
+            ret_value = relaxation_t.disjunction(ret_value, relax_sympy(child, relaxation_t, values, device), device)
         return ret_value
     elif isinstance(expr, Not):
         child = expr.args[0]
-        ret_value = relaxation_t.negation(relax_sympy(child, relaxation_t, values, device))
+        ret_value = relaxation_t.negation(relax_sympy(child, relaxation_t, values, device), device)
         return ret_value
     elif isinstance(expr, Implies):
         children = expr.args
         ret_value = relaxation_t.implication(
             relax_sympy(children[0], relaxation_t, values, device), 
-            relax_sympy(children[1], relaxation_t, values, device))
+            relax_sympy(children[1], relaxation_t, values, device), device)
         return ret_value
     elif isinstance(expr, Symbol):
         symbol_num = extract_int_from_str(str(expr))
@@ -120,15 +119,41 @@ def relax_sympy(expr: BooleanFunction, relaxation_t: FuzzyRelaxation, values: to
         return torch.tensor(1.).to(device)
     else:
         raise ValueError("Expression is invalid")
+
+def max_sat(cnf: BooleanFunction, values: Tensor, device):
+    satisfaction = torch.tensor(0., device=device)
+    for clause in cnf.args:
+        literals = clause.args
+        if isinstance(clause, Symbol):
+            literals = [clause]
+
+        variables = torch.tensor([values[extract_int_from_str(str(lit)) - 1] for lit in literals], device=device)
+        negated = torch.tensor([True if isinstance(lit, Not) else False for lit in literals], device=device)
+        variables = torch.where(negated, 1 - variables, variables)
+        satisfaction += torch.minimum(torch.sum(variables), torch.tensor(1, device=device))
+    return satisfaction
+
+def ndcg_score(relevance_ranking: Sequence[float], optimal_ranking: Sequence[float]=None, k: int=None):
+    if k is None or k > len(relevance_ranking):
+        k = len(relevance_ranking)
+    if optimal_ranking is None:
+        optimal_ranking = np.sort(relevance_ranking)[::-1]
+
+    def dcg_score(relevance_ranking):
+        scores = np.array(relevance_ranking) / np.log2(np.arange(2, len(relevance_ranking) + 2))
+        return np.sum(scores[:k])
+    dcg = dcg_score(relevance_ranking)
+    idcg = dcg_score(np.sort(relevance_ranking)[::-1])
+    return dcg / idcg
+
+
     
 if __name__ == "__main__":
-    test_cnf1 = "./dataset/development/sr_n=0003_pk2=0.30_pg=0.40_t=0_sat=1.dimacs"
-    test_cnf2 = "./dataset/development/sr_n=0003_pk2=0.30_pg=0.40_t=1_sat=1.dimacs"
-    test_cnf3 = "./dataset/development/sr_n=0004_pk2=0.30_pg=0.40_t=3_sat=1.dimacs"
+    test_cnf1 = "./tests/testformula.cnf"
+    formula = dimacs.load_file(test_cnf1)
+    print(formula)
+    print(relax_sympy(formula, relaxation_types['godel'], torch.tensor([.3, .2, .7, .8, 0], dtype=float), torch.device("cpu")))
     
-    test_adj1 = dimacs_to_adjacency(test_cnf1)
-    test_adj2 = dimacs_to_adjacency(test_cnf2)
-    test_adj3 = dimacs_to_adjacency(test_cnf3)
-    
-    torch.set_printoptions(profile='full', linewidth=1000)
-    print(direct_sum([test_adj1, test_adj2, test_adj3]).to_dense())
+    print(max_sat(formula, torch.tensor([.3,.2,.7,.8,.0]), torch.device("cpu")))
+
+    print(ndcg_score([1,1,0,0.5,0], k=3))
